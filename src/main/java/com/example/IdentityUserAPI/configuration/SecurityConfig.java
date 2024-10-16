ee5d7c041cdf92b3d31fa7affa87118591d60145
Tien Pham <pvtienlamk@gmail.com>
2024-10-16 16:17:27 +0700

    Done refacter handl http status code

--- src/main/java/com/example/IdentityUserAPI/configuration/SecurityConfig.java
+++ src/main/java/com/example/IdentityUserAPI/configuration/SecurityConfig.java
@@ -42,7 +42,9 @@ public class SecurityConfig {
         httpSecurity.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer ->
                         jwtConfigurer.decoder(jwtDecoder())
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter())));
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+        );
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
